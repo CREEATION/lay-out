4b97b1a8f8e5fd963ca8fe231f4b6dca56863ca1
Thomas <tcreeten@gmail.com>
2018-10-20 22:47:18 +0200

    ~ Update test.js to clarify what happens

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 var layOut = require('./index.js');
 
+// generates "LAY-OUT.md"
+layOut('example.lf');
+
+// generates "TEST_OUTPUT.md"
 layOut({
   input: 'example.lf',
-  output: 'testme.md'
+  output: 'TEST_OUTPUT.md'
 });
-
-layOut('example.lf');
